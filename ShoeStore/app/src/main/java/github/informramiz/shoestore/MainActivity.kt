3b2f086dcc3854bf75e4483f6f1dd3fcec82b879
Ramiz Raja <informramiz@gmail.com>
2020-04-08 15:46:02 +0500

    feat: Add data binding to main screen

--- ShoeStore/app/src/main/java/github/informramiz/shoestore/MainActivity.kt
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/MainActivity.kt
@@ -2,14 +2,19 @@ package github.informramiz.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import github.informramiz.shoestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
         setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
