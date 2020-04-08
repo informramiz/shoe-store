fe9a6098807efbb1c1d86d8e1bd7d645096ad80c
Ramiz Raja <informramiz@gmail.com>
2020-04-08 22:29:15 +0500

    feat: Create blank login fragment

---
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/onboarding/login/LoginFragment.kt
@@ -0,0 +1,35 @@
+package github.informramiz.shoestore.onboarding.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+
+import github.informramiz.shoestore.R
+import github.informramiz.shoestore.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() =
+            LoginFragment()
+    }
+
+    private val viewModel: LoginViewModel by viewModels()
+    private lateinit var viewBinding: LoginFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.login_fragment, container, false)
+        return viewBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
---
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/onboarding/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package github.informramiz.shoestore.onboarding.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
