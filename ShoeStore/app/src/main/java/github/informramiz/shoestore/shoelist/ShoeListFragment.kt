b95493d557afc18dfffeb888019bf680a75acbb4
Ramiz Raja <informramiz@gmail.com>
2020-04-08 15:27:13 +0500

    feat: Enable data binding

--- ShoeStore/app/src/main/java/github/informramiz/shoestore/shoelist/ShoeListFragment.kt
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/shoelist/ShoeListFragment.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import github.informramiz.shoestore.R
 import github.informramiz.shoestore.ShoeListViewModel
+import github.informramiz.shoestore.databinding.ShoeListFragmentBinding
 
 class ShoeListFragment : Fragment() {
 
@@ -17,12 +19,14 @@ class ShoeListFragment : Fragment() {
     }
 
     private val viewModel: ShoeListViewModel by activityViewModels()
+    private lateinit var viewBinding: ShoeListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.shoe_list_fragment, container, false)
+        viewBinding = DataBindingUtil.inflate(inflater, R.layout.shoe_list_fragment, container, false)
+        return viewBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
