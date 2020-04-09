ed46db36796c9efd20b6f0322d4326cecab94952
Ramiz Raja <informramiz@gmail.com>
2020-04-10 03:15:07 +0500

    feat: Add blank detail fragment

---
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/view/shoedetail/ShoeDetailFragment.kt
@@ -0,0 +1,37 @@
+package github.informramiz.shoestore.view.shoedetail
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+
+import github.informramiz.shoestore.R
+import github.informramiz.shoestore.databinding.ShoeDetailFragmentBinding
+
+class ShoeDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ShoeDetailFragment()
+    }
+
+    private val viewModel: ShoeDetailViewModel by activityViewModels()
+    private lateinit var viewBinding: ShoeDetailFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = ShoeDetailFragmentBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
+        viewBinding.viewModel = viewModel
+    }
+
+}
---
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/view/shoedetail/ShoeDetailViewModel.kt
@@ -0,0 +1,7 @@
+package github.informramiz.shoestore.view.shoedetail
+
+import androidx.lifecycle.ViewModel
+
+class ShoeDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
