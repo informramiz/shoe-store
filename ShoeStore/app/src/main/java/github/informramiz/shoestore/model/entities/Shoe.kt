2f0a455965056d8b4300a18362edf9f25a64869a
Ramiz Raja <informramiz@gmail.com>
2020-04-10 00:29:22 +0500

    feat: Add Shoe entity

---
+++ ShoeStore/app/src/main/java/github/informramiz/shoestore/model/entities/Shoe.kt
@@ -0,0 +1,11 @@
+package github.informramiz.shoestore.model.entities
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Created by Ramiz Raja on 10/04/2020
+ */
+@Parcelize
+data class Shoe(var name: String, var size: Double, var company: String, var description: String,
+                val images: List<String> = mutableListOf()): Parcelable
